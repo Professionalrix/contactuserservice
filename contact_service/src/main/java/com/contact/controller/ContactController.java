9134a6d47d527b8cb0745fbbf37992d038b6b72f
parthgreatwits <79511053+parthgreatwits@users.noreply.github.com>
2021-11-30 10:13:12 +0530

    fifth commit

--- contact_service/src/main/java/com/contact/controller/ContactController.java
+++ contact_service/src/main/java/com/contact/controller/ContactController.java
@@ -26,11 +26,6 @@ public class ContactController {
 		return this.contactService.getContact(userId);
 	}
 	
-	@GetMapping("/users/{userId}")
-	public List<Contact> getContactdds(@PathVariable Long userId){
-		System.out.println("ljlkjlkj ");
-		return this.contactService.getContact(userId);
-	}
 	
 	
 	
